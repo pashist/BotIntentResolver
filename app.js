b0b7a082ef3e6619a0d277f553a71b8b0e4ee583
pavel <mistby@gmail.com>
2017-11-20 20:09:08 +0300

    updated MicrosoftAppId and MicrosoftAppPassword passing from env

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 const connector = new builder.ChatConnector({
-  appId: process.env.MICROSOFT_APP_ID,
-  appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID || process.env.MicrosoftAppId,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD || process.env.MicrosoftAppPassword
 });
 server.post('/api/messages', connector.listen());
 
